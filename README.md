ee83e1c96dd130ed5859f11df3f456c61cec4864
takanami.kodai <kohdai.takanami@gmail.com>
2019-03-05 12:03:02 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Find the smallest missing number in a list 
+
+Input a list and find the smallest number that not including in the list.
+
+リストを受け取り、そのリストの中にない最小の数値を探します。 
+
+
+### Usage
+
+1. git clone
+2. Edit this code as you want to.
+3. run!
+
+
---
+++ missing_nums.py
@@ -0,0 +1,34 @@
+
+
+# Find the smallest missing number in a list
+def findMissingNo(arr, n):
+
+    for i in range(n) :
+
+        if (arr[i] <= 0 or arr[i] > n):
+            continue
+
+        val = arr[i]
+
+    while (arr[val-1] != val):
+        nextval = arr[val-1]
+        arr[val-1] = val
+        val = nextval
+        if (val <= 0 or val > n):
+            break
+
+    for i in range(n):
+        if (arr[i] != i + 1) :
+            return i + 1
+
+    return n + 1
+
+
+if __name__ == '__main__':
+    # sample list
+    arr = [ 2, 3, 7, 6, 8, -1, -10, 15 ]
+    
+    arr_size = len(arr)
+    missing = findMissingNo(arr, arr_size)
+    print( 'The smallest positive', 'missing number is ', missing)
+
